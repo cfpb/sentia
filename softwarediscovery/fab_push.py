b8d1f8e3c8f919ef568386692b2b4e15ae819ebc
lucas.pfaff <lucas.pfaff@excella.com>
2015-02-03 15:09:30 -0500

    Better Fabric Stuff

--- softwarediscovery/fab_push.py
+++ softwarediscovery/fab_push.py
@@ -1,5 +1,37 @@
+import urllib2
+import json
+import sys
 from fabric.api import run
 from fabric.operations import put
+from fabric.context_managers import settings
+
+
+
+def process_all(argv):
+
+    elasticsearch_url = argv[1]
+    edda_url = argv[2]
+    machine_user = argv[3]
+
+    if argv[4] != "key_filename:none":
+      filename = argv[4]
+    else:
+      filename = None
+
+
+    req = urllib2.urlopen(edda_url+'/edda/api/v2/aws/addresses')
+    ipaddresses = json.load(req)
+
+    print "Found " + str(len(ipaddresses)) + " ipaddresses"
+    count =0
+    for machine_ip in ipaddresses:
+        count += 1
+        print str(count)+". SSH into machine_ip: " + machine_ip
+        try:
+            with settings(host_string=machine_ip, user=machine_user, key_filename=filename):
+                task(elasticurl=elasticsearch_url)
+        except:
+            print "Issue connecting"
 
 
 def task(elasticurl=None):
@@ -10,3 +42,17 @@ def task(elasticurl=None):
         run("python /tmp/get_installed_software.py "  )
 
     run("rm /tmp/get_installed_software.py")
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        process_all(sys.argv)
+    else:
+        print "Invalid Arguments"
+
+
+
+
+
+
