9e3aee16d8028ed6ef04f412233e55b11101f0e6
cjfont <cjfont@gmail.com>
2015-05-19 14:53:43 -0400

    Initial working version of server-side API with doc

---
+++ API.md
@@ -0,0 +1,20 @@
+### Sentia API
+
+- ##### Instances
+
+  Return list of instances across all or select list of providers.
+
+  - `/api/instances` - List all instances from all providers
+  - `/api/instances/<providers>` - List instances from comma-delimited list of providers
+  
+  ###### Result
+  
+  ```
+  [
+    {id: 'inst1_id', name: 'inst1_name', privateIp: '10.10.10.10', tags: {}},
+  ]
+  ```
+  
+- ##### Subnets
+
+  *etc. etc.*
--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,66 @@
 var fs = require('fs');
 var path = require('path');
 
+var async = require('async');
 var express = require('express');
+var _ = require('underscore');
 var router = express.Router();
 
 // load data providers
 var providers = {};
-fs.readdir(path.join(__dirname, '../providers'), function(err, file) {
+fs.readdir(path.join(__dirname, '../providers'), function(err, contents) {
   if (err) throw Error('Unable to load data providers');
-  var pname = path.basename(file);
-  try {
-    providers[path.basename(file)] = 
-  } catch(e) {
-    throw Error('Error while loading data provider \'' + pname + '\': ' + e.toString());
-  }
-  
+  _.each(contents, function(file) {
+    var pname = _.first(path.basename(file).split('.'));
+    try {
+      providers[pname] = require(path.join(__dirname, '../providers', file));
+    } catch(e) {
+      throw Error('Error while loading data provider \'' + pname + '\': ' + e.toString());
+    }  
+  });
 });
 
+// --------------------------------------------------------------------------------------
+// Define API routes
 
-// define API routes
-router.get('/instances', function(req, res, next) {
-    res.send({test:123});
+// List of supported API calls
+var supported_actions = ['instances']
+
+// Apply API call to ALL providers
+router.get('/:action', function(req, res, next) {
+  async.map(_.pairs(providers), function(pair, cb) {
+    var id = pair[0];
+    var provider = pair[1];
+    if (supported_actions.indexOf(req.params.action) === -1) return cb('Unsupported API call: ' + req.params.action);
+    if (!_.isFunction(provider[req.params.action])) return; // skip if not supported
+    provider[req.params.action](req.query, function(err, res) {
+      if (err) return cb(err);
+      cb(null, _.map(res, function(rec) {return _.assign(rec, {source: id})}));
+    });
+  }, function(err, result) {
+    if (err) return res.status(400).send({error: err});
+    var final_result = _.flatten(result, true);
+    res.send(final_result);
+  });
 });
 
-// error handler
-function api_error(msg) {
-  
-}
+// Apply API call to filtered list of providers
+router.get('/:action/:providers', function(req, res, next) {
+  async.map(_.pairs(providers), function(pair, cb) {
+    var id = pair[0];
+    var provider = pair[1];
+    if (req.params.providers && req.params.providers.split(',').indexOf(id) === -1) return; // skip if not in filter
+    if (supported_actions.indexOf(req.params.action) === -1) return cb('Unsupported API call: ' + req.params.action);
+    if (!_.isFunction(provider[req.params.action])) return; // skip if not supported
+    provider[req.params.action](req.query, function(err, res) {
+      if (err) return cb(err);
+      cb(null, _.map(res, function(rec) {return _.assign(rec, {source: id})}));
+    });
+  }, function(err, result) {
+    if (err) return res.status(400).send({error: err});
+    var final_result = _.flatten(result, true);
+    res.send(final_result);
+  });
+});
 
 module.exports = router;
